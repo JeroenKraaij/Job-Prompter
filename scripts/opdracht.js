6fbfd40c63e66b629b54ce905abd9f3eb66e149c
JeroenKraaij <jeroen@jeroenkraaij.com>
2023-10-09 20:45:38 +0200

    Final Assesment

--- scripts/opdracht.js
+++ scripts/opdracht.js
@@ -36,25 +36,3 @@ switch (userInput) {
         console.error(`Ongeldige keuze`);
 }
 
-//opdracht 3 //
-
-switch(jobChoice){
-    case '0':
-        console.log('Je koos ' + departments.marketing.jobs[0].title + ' Een uitdagende rol!' + departments.marketing.jobs[0].description);
-        break;
-    case '1':
-        console.log('Je koos ' + departments.marketing.jobs[1].title + ' Een uitdagende rol!' + departments.marketing.jobs[1].description);
-        break;
-    case '2':
-        console.log('Je koos ' + departments.marketing.jobs[2].title + ' Een uitdagende rol!' + departments.marketing.jobs[2].description);
-        break;
-    case '3':
-        console.log('Je koos ' + departments.marketing.jobs[3].title + ' Een uitdagende rol!' + departments.marketing.jobs[3].description);
-        break;
-    case '4':
-        console.log('Je koos ' + departments.marketing.jobs[4].title + ' Een uitdagende rol!' + departments.marketing.jobs[4].description);
-        break;
-    default:
-        console.error('Mag niet')
-
-}
